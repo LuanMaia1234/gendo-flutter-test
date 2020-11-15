7478d0fe559b09674555add5ca2f2a88b778b3c2
LuanMaia1234 <luanmaia05@hotmai.com>
2020-11-15 17:25:20 -0300

    ajuste

--- lib/api/github_api.dart
+++ lib/api/github_api.dart
@@ -4,17 +4,15 @@ import 'package:http/http.dart' as http;
 //Classe para fazer requisição para api GitHub
 class GitHubApi {
   final String gitUrl = 'https://api.github.com';
-  final String token = 'b496eb0d70d4df9179d4e173da7a1de94f344133';
+  final String token = '';
 
   //Traz os repositórios do usuário
   getRepositories({String search}) async {
     http.Response response;
     if (search == null) {
-      response = await http.get('$gitUrl/users/LuanMaia1234/repos',
-          headers: {'Authorization': 'token $token'});
+      response = await http.get('$gitUrl/users/LuanMaia1234/repos');
     } else {
-      response = await http.get('$gitUrl/repos/LuanMaia1234/$search',
-          headers: {'Authorization': 'token $token'});
+      response = await http.get('$gitUrl/repos/LuanMaia1234/$search');
     }
 
     if (response.statusCode == 200) {
@@ -31,15 +29,13 @@ class GitHubApi {
 
   //Traz os dados do usuário
   Future<Map<String, dynamic>> getUser() async {
-    final response = await http.get('$gitUrl/users/LuanMaia1234',
-        headers: {'Authorization': 'token $token'});
+    final response = await http.get('$gitUrl/users/LuanMaia1234');
     return json.decode(response.body);
   }
 
   //Traz os favoritos do usuário
   getStarred({String search}) async {
-    final response = await http.get('$gitUrl/users/LuanMaia1234/starred',
-        headers: {'Authorization': 'token $token'});
+    final response = await http.get('$gitUrl/users/LuanMaia1234/starred');
     if (response.statusCode == 200) {
       return json.decode(response.body);
     } else {
